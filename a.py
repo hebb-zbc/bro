ce662d326b2d7894f5e148e6206d652d897aefc3
henrik bruhn <hebb@zbc.dk>
2024-03-15 06:50:08 +0100

    Add files via upload

---
+++ a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import turtle
+t = turtle.Turtle()
+
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(150)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(150)
+
+#turtle.exitonclick()
+turtle.done()
